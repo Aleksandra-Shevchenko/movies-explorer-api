933a5e9abf9e522752e3dcbefd142ffb03deb09a
Aleksandra-Shevchenko <shevdorosh@yandex.ru>
2021-05-08 12:16:33 +0300

    Рабочая версия 1

---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+// код для авторизации запроса
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config');
+const AuthError = require('../errors/authError');
+
+const auth = (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return next(new AuthError('Токен остутствует или некорректен'));
+  }
+
+  let payload;
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    next(new AuthError('Токен не верифицирован, авторизация не пройдена'));
+  }
+
+  req.user = payload;
+  return next();
+};
+
+module.exports = auth;
